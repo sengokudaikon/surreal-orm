d9c2708707206219fdbc684f522d7871f47d22d5
oyelowo <oyelowooyedayo@gmail.com>
2024-08-20 07:56:11 -0400

    Add some notes

--- query-builder/src/types/maybe.rs
+++ query-builder/src/types/maybe.rs
@@ -13,11 +13,13 @@ pub enum Maybe<T> {
 
 impl<T> Maybe<T> {
     /// Checks if the value is present
+    // Consider changing this to `is_present` or `is_serialized` or `is_included` or `is_set` to avoid confusion with `Option::is_some
     pub fn is_some(&self) -> bool {
         matches!(self, Maybe::Some(_))
     }
 
     /// Checks if the value is absent
+    // Consider changing this to `is_absent` or `is_skipped` or `is_not_set` to avoid confusion with `Option::is_none
     pub fn is_none(&self) -> bool {
         matches!(self, Maybe::None)
     }
